d5eb89a8ba991c20a9d03a2565943d44e2f8e024
lnedwek <lnedwek@gmail.com>
2025-09-09 20:46:45 -0700

    Add function to generate self-portrait

---
+++ app.py
@@ -0,0 +1,19 @@
+# Use functions to create self-portrait 
+
+def create_self_portrait():
+    self_portrait='''
+                   11111111
+                11111111111111
+            1111111        1111111
+            11111   _    _   11111
+            11111  1_1 -1_1  11111
+            11111     '      11111
+            11111    1_1     11111
+             1111            1111
+              111___1    1___111
+               11___1    1___11
+                1            1
+    '''
+    print(self_portrait)
+
+create_self_portrait()
