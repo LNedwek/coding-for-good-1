9cc70471365b44b798794493a354c1ed15d9939c
lnedwek <lnedwek@gmail.com>
2025-10-05 19:48:06 -0700

    We wrote code to get the program to open a specific file

--- README.md
+++ README.md
@@ -4,8 +4,8 @@ First Coding for good senior badge
 ## Goals
 - [x] Use functions to create self-portrait 
 - [x] write code to create a portrait
-    - Allow user to prompt to store a portait
-    - Allow user to prompt to retrieve a portrait by name
-- learn about computer logic
-- explore "IF" statements
-- use computer logic to create a quiz show
+    - [ ] Allow user to prompt to store a portait
+    - [ ] Allow user to prompt to retrieve a portrait by name
+- [x] learn about computer logic
+- [x] explore "IF" statements
+- [ ] use computer logic to create a quiz show
--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 # Use functions to create self-portrait 
 import sys
+import os
+
+current_directory = os.getcwd()
+print("Current Directory:", current_directory)
+portrait_directory = f"{current_directory}\data"
 
 def create_self_portrait():
     self_portrait='''
@@ -17,29 +22,37 @@ def create_self_portrait():
     '''
     print(self_portrait)
 
-# Takes in user input
-#  - Allow user to store portrait in a file registered to their name
-def store_portrait(name:str, portrait_bytes:bytes):
+def store_portrait(name:str, portrait_bytes:str):
     print(f"Storing portrait for {name}")
+    with open(f"{portrait_directory}\{name}", "w", encoding="utf-8") as f:
+        f.write(portrait_bytes)
 
+#  TODO - Allow user to store portrait in a file registered to their name
 
-#  - Allow user to view their stored portrait
 def view_portrait(name:str):
     print(f"Viewing portrait for {name}")
-#  - Allow user to View awesome ASCII portrait of Leah
+    with open(f"{portrait_directory}\{name}", "r", encoding="utf-8") as f:
+        print(f.read())
+#  TODO - Allow user to view a stored portrait
 
 if __name__ == "__main__":
     # sys.argv is a list of command line arguments
     # sys.argv[0] is the file being run
     # sys.argv[1] is the first argument after the file name (e.g. - `leah` in `py app.py "leah"`)
+
     file_name = sys.argv[0]
     print(f"Running {file_name} with arguments {sys.argv[1:]}")
     if(len(sys.argv) > 1 and sys.argv[1] == "leah"):
         create_self_portrait()
     if(len(sys.argv) > 2 and sys.argv[1] == "save"):
-        # TODO implement photo capture
         name = sys.argv[2]
-        store_portrait(name, b"portrait_bytes")
+        data =sys.argv[3]
+        store_portrait(name, data)
+    if(len(sys.argv) > 2 and sys.argv[1] == "view"):
+        name = sys.argv[2]
+        view_portrait(name)
+
+# https://www.geeksforgeeks.org/python/writing-to-file-in-python/ 
 
 # {} - Squiggly brackets - dictionaries, string formatting
 # [] - Square brackets - lists, indexing
